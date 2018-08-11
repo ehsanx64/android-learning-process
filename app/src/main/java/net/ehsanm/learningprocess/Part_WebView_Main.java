15e468ce5a570dc0c19370ad1e41d1544e03c22b
Ehsan Mohammadi <ehsanx64@gmail.com>
2018-08-11 15:24:10 +0430

    Main WebView added to WebView module

--- app/src/main/java/net/ehsanm/learningprocess/Part_WebView_Main.java
+++ app/src/main/java/net/ehsanm/learningprocess/Part_WebView_Main.java
@@ -6,12 +6,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class Part_WebView_Main extends AppCompatActivity {
+    // This is our main WebView
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_part__web_view__main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -24,6 +30,26 @@ public class Part_WebView_Main extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        doWebviewStuff();
     }
 
+    private void doWebviewStuff() {
+        String googleUrl = "https://www.google.com";
+
+        // Get it
+        webView = findViewById(R.id.webview_main);
+
+        // Enable JavaScript
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            public void onRecievedError(WebView wv, int errorCode, String description, String failingUrl) {
+                Toast.makeText(wv.getContext(), "HTTP Error" + errorCode + ": " + description, Toast.LENGTH_LONG);
+            }
+        });
+
+        // Load this url
+        webView.loadUrl(googleUrl);
+    }
 }
