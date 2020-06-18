9597546b7a15fd6f92cc5bee8637ec1be5f8c4f2
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-06-19 00:07:02 +0430

    Hide the action bar in webview activity

--- app/src/main/java/net/ehsanm/learningprocess/Part_WebView_Main.java
+++ app/src/main/java/net/ehsanm/learningprocess/Part_WebView_Main.java
@@ -3,6 +3,7 @@ package net.ehsanm.learningprocess;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,20 +23,14 @@ public class Part_WebView_Main extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
         doWebviewStuff();
     }
 
     private void doWebviewStuff() {
-        String googleUrl = "https://www.google.com";
+        String googleUrl = "https://google.com";
 
         // Get it
         webView = findViewById(R.id.webview_main);
